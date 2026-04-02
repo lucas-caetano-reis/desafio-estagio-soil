a264c5161595c10299f1e5e3ee22486915d61f9e
Lucas <lucas.caetano.reis@hotmail.com>
2026-04-01 21:48:17 -0300

    Setup inicial do Next.js

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Soil - Esta é uma landing page com Next.js",
+  description: "Essa seria a descrição da página",
+};
+
+type RootLayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <main className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold text-amber-500">Olá mundo!</h1>
+    </main>
+  );
+}
