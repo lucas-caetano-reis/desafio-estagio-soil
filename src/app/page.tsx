c128f58d29e0cf3a95616c1def79db0d73c147cd
Lucas <lucas.caetano.reis@hotmail.com>
2026-04-03 15:12:19 -0300

    Criando a seção HeroSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
 import Container from "@/components/Container";
 import Footer from "@/components/Footer";
-import Header from "@/components/Header";
+import HeroSection from "@/sections/HeroSection";
 
 export default function HomePage() {
   return (
     <Container>
-      <Header />
-
+      <HeroSection />
       <main className="flex min-h-screen items-center justify-center">
         <h1 className="text-3xl font-bold text-amber-500">Olá mundo!</h1>
       </main>
-
       <Footer />
     </Container>
   );
---
+++ src/sections/HeroSection/index.tsx
@@ -0,0 +1,13 @@
+import clsx from "clsx";
+import Header from "@/components/Header";
+
+export default function HeroSection() {
+  return (
+    <>
+      <Header />
+      <h1 className="py-8 text-center text-6xl font-bold">
+        Esta é a HeroSection
+      </h1>
+    </>
+  );
+}
