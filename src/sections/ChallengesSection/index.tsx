1a02359e57e3cd556639a32f82e48f66a2cc3e7c
Lucas <lucas.caetano.reis@hotmail.com>
2026-06-27 15:13:16 -0300

    refactor: Ajustei a altura da seção de soluções.

--- src/sections/ChallengesSection/index.tsx
+++ src/sections/ChallengesSection/index.tsx
@@ -8,7 +8,8 @@ export default function ChallengesSection() {
     <section
       id="desafio"
       className={clsx(
-        "flex min-h-[calc(100svh-var(--header-height))] flex-col items-center",
+        "flex min-h-[calc(100svh-var(--header-height))] flex-col",
+        "items-center justify-center",
         "bg-[#fffdf9]",
         "px-8 py-8",
         "sm:px-12 sm:py-10",
--- src/sections/SolutionSection/index.tsx
+++ src/sections/SolutionSection/index.tsx
@@ -8,10 +8,11 @@ export default function SolutionSection() {
     <section
       id="solucao"
       className={clsx(
-        "flex flex-col lg:flex-row",
+        "flex min-h-[calc(100svh-var(--header-height))] flex-col lg:flex-row",
         "items-center justify-center",
         "bg-[#253517] text-white",
-        "gap-12 lg:gap-16 xl:gap-20",
+        "gap-y-12 lg:gap-y-0",
+        "lg:gap-x-16 xl:gap-x-20",
         "px-8 py-8",
         "sm:px-12 sm:py-10",
         "md:px-16 md:py-12",
@@ -22,10 +23,11 @@ export default function SolutionSection() {
         <div
           className={clsx(
             "flex items-center gap-2",
-            "text-sm sm:text-base text-[#a5bf55] font-semibold",
+            "text-[#a5bf55] font-semibold tracking-tight",
+            "text-sm sm:text-lg xl:text-xl",
           )}
         >
-          <span className="h-px w-4 sm:w-6 bg-[#a5bf55]" />
+          <span className="h-px sm:h-0.5 w-4 sm:w-6 bg-[#a5bf55]" />
           <span>Conectividade total + inteligência artificial</span>
         </div>
 
@@ -41,7 +43,7 @@ export default function SolutionSection() {
         <p
           className={clsx(
             "mt-5 text-white/92 leading-tight",
-            "text-base sm:text-lg md:text-xl lg:text-2xl",
+            "text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl",
           )}
         >
           Com a Soil, você controla seus pivôs de qualquer lugar, mesmo offline.
@@ -53,7 +55,7 @@ export default function SolutionSection() {
             "mt-8 space-y-6 pl-6",
             "marker:text-[#a5bf55]",
             "font-semibold leading-tight",
-            "text-sm sm:text-base md:text-lg lg:text-xl",
+            "text-sm sm:text-base md:text-lg lg:text-xl xl:text-[22px]",
           )}
         />
       </article>
@@ -65,7 +67,7 @@ export default function SolutionSection() {
         height={512}
         classname={clsx(
           "bg-[#31431f]",
-          "max-w-3xs sm:max-w-2xs md:max-w-xs lg:max-w-sm xl:max-w-md",
+          "max-w-2xs sm:max-w-xs md:max-w-sm lg:max-w-lg",
           "aspect-512/512",
           "rounded-4xl border-4 border-[#a5bf55]/90",
         )}
